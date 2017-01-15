e8ea250fbaabe2869bf09ce40a6555b8ca713a52
sudeep93 <sud0@live.com>
2017-01-15 11:23:49 +0530

    last

--- proj2frontend/WebContent/_chat/chatcontroller.js
+++ proj2frontend/WebContent/_chat/chatcontroller.js
@@ -1,14 +1,20 @@
-app.controller("ChatCtrl", function($scope,$rootScope ,ChatService) {
+app.controller("ChatCtrl", function($scope,$rootScope,$cookieStore ,ChatService) {
     $scope.messages = [];
     $scope.message = "";
     $scope.max = 140;
     
+	if($rootScope.currentUser==undefined)
+		$rootScope.currentUser=$cookieStore.get('currentUser')
+    
     $scope.addMessage = function() {
       ChatService.send($rootScope.currentUser.username + " : " + $scope.message);
+      $rootScope.currentUser=$cookieStore.get('currentUser');
+      
       $scope.message = "";
     };
     
     ChatService.receive().then(null, null, function(message) {
+    	$rootScope.currentUser=$cookieStore.get('currentUser')
       $scope.messages.push(message);
     });
   });
